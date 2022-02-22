2bd4d34e9d53eb841e7dfc793b8c92b965736568
Ashasullia <99865138+Ashasullia@users.noreply.github.com>
2022-02-22 10:41:35 +0530

    Add files via upload

---
+++ lengtharray2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+structNode{
+	int Value;
+	struct Node*next;
+};
+structNode*head=NULL;
+structNode*sHead=NULL;
+structNode*temp=NULL;
+voidinsert(int new data){
+	struct 
+
